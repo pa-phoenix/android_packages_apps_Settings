16dd3c10981261173048cdc237bba52d43679c3a
Danny Lin <danny@kdrag0n.dev>
2021-07-14 22:51:15 +0800

    Settings: Add sliders for display color balance adjustment
    These sliders allow the user to adjust RGB color balance using our
    simple ColorDisplayManager transforms.
    
    Requires frameworks/base commit: "display: Add simple RGB color balance transform"
    
    [ghostrider-reborn]
    - Switch to CustomSecureSeekBarPreference
    - Show color settings on all devices
    
    Change-Id: I8205ab6e0c354ab77b58d55c5ca2c51fb473cf30
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- src/com/android/settings/display/ColorModePreferenceController.java
+++ src/com/android/settings/display/ColorModePreferenceController.java
@@ -31,16 +31,20 @@ public class ColorModePreferenceController extends BasePreferenceController {
 
     @Override
     public int getAvailabilityStatus() {
-        final int[] availableColorModes = mContext.getResources().getIntArray(
+        return AVAILABLE; /* final int[] availableColorModes = mContext.getResources().getIntArray(
                 com.android.internal.R.array.config_availableColorModes);
-        return availableColorModes.length == 0 ? UNSUPPORTED_ON_DEVICE : 
+        return availableColorModes.length == 0 ? UNSUPPORTED_ON_DEVICE :
                 mContext.getSystemService(ColorDisplayManager.class).isDeviceColorManaged()
                 && !ColorDisplayManager.areAccessibilityTransformsEnabled(mContext) ?
-                AVAILABLE : DISABLED_FOR_USER;
+                AVAILABLE : DISABLED_FOR_USER; */
     }
 
     @Override
     public CharSequence getSummary() {
+        if (!getColorDisplayManager().isDeviceColorManaged()) {
+            return mContext.getText(R.string.color_mode_balance_summary);
+        }
+
         final int colorMode = getColorDisplayManager().getColorMode();
         if (colorMode == ColorDisplayManager.COLOR_MODE_AUTOMATIC) {
             return mContext.getText(R.string.color_mode_option_automatic);
